31b9d662ee9d7f9f7ff79bef4ea4b5c0e45539e2
Patrick Weiss <patrick.weiss@officeone.team>
2019-10-30 10:08:21 +0100

    04-04 rectangle

--- module1/04-04-Canvas-Basic-Animations/script.js
+++ module1/04-04-Canvas-Basic-Animations/script.js
@@ -1,5 +1,7 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+
+/*
 ctx.fillStyle = "#FF0000";
 ctx.fillRect(0,0,150,150);
 var color = {
@@ -20,6 +22,32 @@ function updateCanvas(){
   ctx.fillStyle = color.rgb();
   ctx.fillRect(0,0,150,150);
   window.requestAnimationFrame(updateCanvas);
+}
+
+window.requestAnimationFrame(updateCanvas);
+*/
+
+ctx.fillStyle = "#FF0000";
+var y1 = 0;
+var y2 = 0;
+var y3 = 0;
+var dy3 = 3;
+
+function clearCanvas() {
+  ctx.clearRect(0,0,480,270);
+}
+
+function updateCanvas(){
+  y1 += 1;
+  y2 += 2;
+  y3 += dy3;
+
+  if (y3>270)dy3=-3;
+  if (y3<0) dy3=3;
+  clearCanvas();
+  ctx.fillRect( 50,y1,50,50);
+  ctx.fillRect(150,y2,50,50);
+  ctx.fillRect(250,y3,50,50);  window.requestAnimationFrame(updateCanvas);
 }
 
 window.requestAnimationFrame(updateCanvas);

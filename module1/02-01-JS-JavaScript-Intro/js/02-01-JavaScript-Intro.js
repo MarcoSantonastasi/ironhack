8aaf2332c431f25ea45acbf13f838c53a79198e6
Patrick Weiss <patrick.weiss@officeone.team>
2019-11-12 19:38:16 +0100

    02-01-js-intro

---
+++ module1/02-01-JS-JavaScript-Intro/js/02-01-JavaScript-Intro.js
@@ -0,0 +1,49 @@
+console.log("wir geben etwas anderes in der Browser Console aus");
+// Kommentar
+/*
+ langes Kommentar
+*/
+
+//let vorname;
+let vorname,alter,email;
+//show the error in console
+
+vorname = "Herbert";
+
+console.log(vorname);
+let kontostand = 3000;
+console.log(kontostand);
+
+let a; 
+let color;
+let _private;
+let $button;
+let getTop10;
+let a_large_name;
+// let a-large-name; not allowed;
+let thisWayIsCalledCamelCase;
+let π;
+
+//keywords not allowed
+//let for = 5;
+
+let lieblingsEssen = "Pizza";
+
+console.log(lieblingsEssen);
+lieblingsEssen = "Pasta";
+console.log(lieblingsEssen);
+
+console.log("Wert:",lieblingsEssen,"Typ:", typeof lieblingsEssen);
+
+lieblingsEssen=42;
+
+console.log("Wert:",lieblingsEssen,"Typ:", typeof lieblingsEssen);
+
+const name = "Ana";
+name = "Marina";
+
+//const price;
+
+
+
+

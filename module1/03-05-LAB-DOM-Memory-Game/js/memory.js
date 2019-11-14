6ebf8a7ccd7b7ec402042d12b7622fda562a0609
MarcoSantonastasi <marco.santonastasi@altesis-pro.com>
2019-11-14 13:45:28 +0100

    fix onclick arrow function

--- module1/03-05-LAB-DOM-Memory-Game/js/memory.js
+++ module1/03-05-LAB-DOM-Memory-Game/js/memory.js
@@ -1,35 +1,36 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
     this.pickedCards = [];
     this.pairsClicked = 0;
     this.pairsGuessed = 0;
   }
   shuffleCards() {
-    var currentIndex = this.cards.length, temporaryValue, randomIndex;
+    var currentIndex = this.cards.length,
+      temporaryValue,
+      randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = this.cards[currentIndex];
-    this.cards[currentIndex] = this.cards[randomIndex];
-    this.cards[randomIndex] = temporaryValue;
+      // And swap it with the current element.
+      temporaryValue = this.cards[currentIndex];
+      this.cards[currentIndex] = this.cards[randomIndex];
+      this.cards[randomIndex] = temporaryValue;
+    }
   }
-  }
 
-  cardClicked (card) {
-    console.log('Card clicked: ', card);
+  cardClicked(card) {
+    console.log("Card clicked: ", card);
 
     if (this.pickedCards.length < 2) {
       this.pickedCards.push(card);
       this.flipCard(card);
-      }
-    
+    }
+
     if (this.pickedCards.length == 2) {
       if (this.checkIfPair()) this.isFinished();
       else this.resetPickedCards();
@@ -37,17 +38,17 @@ class MemoryGame {
   }
 
   resetPickedCards() {
-    setTimeout( () => {
-    this.pickedCards.forEach(card => this.flipCard(card));
-    this.pickedCards = [];
-    }, 1000)
+    setTimeout(() => {
+      this.pickedCards.forEach(card => this.flipCard(card));
+      this.pickedCards = [];
+    }, 1000);
   }
 
   flipCard(card) {
-    card.childNodes.forEach( child => {
-    child.classList.toggle("back");
-    console.log('Card flipped: ', child);
-  })
+    card.childNodes.forEach(child => {
+      child.classList.toggle("back");
+      console.log("Card flipped: ", child);
+    });
   }
 
   checkIfPair() {
@@ -58,13 +59,14 @@ class MemoryGame {
     if (card1 === card2) {
       this.pairsGuessed += 1;
       this.pickedCards = [];
-      return true
+      return true;
     } else {
       return false;
     }
   }
+
   isFinished() {
-    if (this.pairsGuessed == this.cards.length/2) return true;
-    else return false
+    if (this.pairsGuessed == this.cards.length / 2) return true;
+    else return false;
   }
 }
